7235dec20d91f3e20e5c3ce48b3f0128bf8ea1c2
Trevin <132922589+T-Crypt@users.noreply.github.com>
2023-10-05 16:59:03 -0600

    refactored install-hypr

--- install-hypr.sh
+++ install-hypr.sh
@@ -1,39 +1,5 @@
 #!/bin/bash
 
-# The follwoing will attempt to install all needed packages to run Hyprland
-# This is a quick and dirty script there are no error checking
-# This script is meant to run on a clean fresh system
-#
-# Below is a list of the packages that would be installed
-#
-# hyprland: This is the Hyprland compositor
-# kitty: This is the default terminal
-# waybar: Waybar now has hyprland support
-# swaybg: This is used to set a desktop background image
-# swaylock-effects: This allows for the locking of the desktop its a fork that adds some editional visual effects
-# wofi: This is an application launcher menu
-# wlogout: This is a logout menu that allows for shutdown, reboot and sleep
-# mako: This is a graphical notification daemon
-# thunar: This is a graphical file manager
-# ttf-jetbrains-mono-nerd: Som nerd fonts for icons and overall look
-# noto-fonts-emoji: fonts needed by the weather script in the top bar
-# polkit-gnome: needed to get superuser access on some graphical appliaction
-# python-requests: needed for the weather module script to execute
-# starship: allows to customize the shell prompt
-# swappy: This is a screenshot editor tool
-# grim: This is a screenshot tool it grabs images from a Wayland compositor
-# slurp: This helps with screenshots, it selects a region in a Wayland compositor
-# pamixer: This helps with audio settings such as volume
-# brightnessctl: used to control monitor bright level
-# gvfs: adds missing functionality to thunar such as automount usb drives
-# bluez: the bluetooth service
-# bluez-utils: command line utilities to interact with bluettoth devices
-# lxappearance: used to set GTK theme
-# xfce4-settings: set of tools for xfce, needed to set GTK theme
-# dracula-gtk-theme: the Dracula theme, it fits the overall look and feel
-# dracula-icons-git" set of icons to go with the Dracula theme
-# xdg-desktop-portal-hyprland: xdg-desktop-portal backend for hyprland
-
 #### Check for yay ####
 ISYAY=/sbin/yay
 if [ -f "$ISYAY" ]; then 
@@ -42,18 +8,6 @@ if [ -f "$ISYAY" ]; then
 else 
     echo -e "yay was not located, please install yay. Exiting script.\n"
     exit 
-fi
-
-### Disable wifi powersave mode ###
-read -n1 -rep 'Would you like to disable wifi powersave? (y,n)' WIFI
-if [[ $WIFI == "Y" || $WIFI == "y" ]]; then
-    LOC="/etc/NetworkManager/conf.d/wifi-powersave.conf"
-    echo -e "The following has been added to $LOC.\n"
-    echo -e "[connection]\nwifi.powersave = 2" | sudo tee -a $LOC
-    echo -e "\n"
-    echo -e "Restarting NetworkManager service...\n"
-    sudo systemctl restart NetworkManager
-    sleep 3
 fi
 
 ### Install all of the above pacakges ####
@@ -83,24 +37,14 @@ if [[ $CFG == "Y" || $CFG == "y" ]]; then
     echo -e "Copying config files...\n"
     cp -R hypr ~/.config/
     cp -R kitty ~/.config/
-    cp -R mako ~/.config/
     cp -R waybar ~/.config/
     cp -R swaylock ~/.config/
-    cp -R wofi ~/.config/
+    cp -R rofi ~/.config/
+    cp -R wlogout ~/.config/
     
     # Set some files as exacutable 
     chmod +x ~/.config/hypr/xdg-portal-hyprland
     chmod +x ~/.config/waybar/scripts/waybar-wttr.py
-fi
-
-### Install teh starship shell ###
-read -n1 -rep 'Would you like to install the starship shell? (y,n)' STAR
-if [[ $STAR == "Y" || $STAR == "y" ]]; then
-    # install the starship shell
-    echo -e "Updating .bashrc...\n"
-    echo -e '\neval "$(starship init bash)"' >> ~/.bashrc
-    echo -e "copying starship config file to ~/.confg ...\n"
-    cp starship.toml ~/.config/
 fi
 
 ### Install software for Asus ROG laptops ###
