9514a91eb530bc2428071355477a6d7501170125
Trevin <132922589+T-Crypt@users.noreply.github.com>
2023-11-01 12:45:56 -0600

    Update thunar_wall.py

--- .configs/hypr/scripts/thunar_wall.py
+++ .configs/hypr/scripts/thunar_wall.py
@@ -15,7 +15,6 @@ os.system(f'cp {image_path} ~/.config/swww/wallpaper.rofi')
 os.system(f'cp ~/.cache/wal/colors-hyprland.conf ~/.config/hypr/colors-hyprland.conf') # hyprland
 os.system(f'cp ~/.cache/wal/colors-waybar.css ~/.config/waybar/') # waybar
 os.system(f'cp ~/.cache/wal/colors-kitty.conf ~/.config/kitty/') # kitty also needed to apply the colorscheme to all new windows after wallpaper change
-os.system(f'cp ~/.cache/wal/colors-waybar.css ~/.config/wlogout/color-wlogout.css') # wlogout
 os.system(f'cp ~/.cache/wal/colors-swaylock ~/.config/swaylock/config') # swaylock
 os.system(f'cp ~/.cache/wal/colors-cava ~/.config/cava/config') # cava
 os.system(f'cp ~/.cache/wal/colors-rofi.rasi ~/.config/rofi/colors/colors-rofi.rasi') # rofi
